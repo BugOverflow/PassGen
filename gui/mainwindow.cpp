a475cd94ec923ebbedc2308fa2d58b1120a59af3
Valery Agishev <phaseowner@gmail.com>
2016-02-27 00:24:08 +0600

    Набросок примерного расположения элементов GUI

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -6,9 +6,35 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->actionExit, SIGNAL(triggered(bool)), QApplication::instance(), SLOT(quit()));
+    connect(ui->tabWidget, SIGNAL(currentChanged(int)), this, SLOT(onTabChanged(int)));
+
+    ui->lineEditResult->setDisabled(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::onTabChanged(int index)
+{
+    switch (index)
+    {
+        case RANDOM_TAB:
+            ui->statusBar->showMessage("Choose strength and size, and ready to go!", 5000);
+            break;
+        case TEMPLATE_TAB:
+            ui->statusBar->showMessage("Enter your template, and go on!", 5000);
+            break;
+    }
+}
+
+void MainWindow::showResult(QString result)
+{
+    ui->lineEditResult->setEnabled(true);
+    ui->lineEditResult->setText(result);
+    if (ui->actionCopy_to_Clipboard->isChecked())
+        QApplication::clipboard()->setText(result);
 }
--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -2,10 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QClipboard>
 
 namespace Ui {
 class MainWindow;
 }
+
+enum Tabs
+{
+    RANDOM_TAB = 0,
+    TEMPLATE_TAB = 1
+};
 
 class MainWindow : public QMainWindow
 {
@@ -14,6 +21,10 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void onTabChanged(int index);
+    void showResult(QString result);
 
 private:
     Ui::MainWindow *ui;
