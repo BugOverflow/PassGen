950ddd903ad45768363e4a0c2917690386891c1e
Roman Mingazeev <Direnol@yandex.ru>
2016-04-18 12:02:16 +0600

    Add Hash and some gen

--- src/gen.c
+++ src/gen.c
@@ -1,40 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int main()
-{
-int kol;
-int k=1;
-char A[]={'0','1','2','3','4','5','6','7','8','9'};
-char B[]={'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
-char C[]={'!','@','#','$','%','^','&','*','(',')'};
-char D[]={'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'};
-int i;
-int a;
-srand(time(NULL));
-printf ("введите количество символов: ");
-scanf ("%d",&kol);
+#include "gen.h"
 
-for (k;k<=kol;k++)
+char *generator_template(char* seed, char *input)
 {
-	i=random() % 4 +1;
-	if (i==1) {
-		a=random() % 9 +0;
-		printf ("%c",A[a]);
-	}
-        else if (i==2) {
-                a=random() % 25 +0;
-                printf ("%c",B[a]);
+	char A[] = {'0','1','2','3','4','5','6','7','8','9'};
+	char B[] = {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
+	char C[] = {'!','@','#','$','%','^','&','*','(',')'};
+	char D[] = {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'};
+	
+	char *out = malloc(sizeof(char) * strlen(input));
+    int i;
+
+    for (i = 0; i < strlen(input); i++) {
+        switch (inputp[i]) {
+            case C:
+                out[i] = B[];
+                break;
+            case c:
+                out[i] = D[];
+                break;
+            case d:
+                out[i] = A[];
+                break;
+            case s:
+                out[i] =C[];
+            
         }
-        else if (i==3) {
-                a=random() % 9 +0;
-                printf ("%c",C[a]);
-        }
-        else if (i==4) {
-                a=random() % 25 +0;
-                printf ("%c",D[a]);
-        }
+    }
+	return out;
 }
-printf ("\n");
-return 0;
+
+char *generator_default(char* seed, char *input, int complexity)
+{
+}
+
+char *hash(char *input_seed);
+{
+    md5_state_t HASH;
+    md5_byte_t digest[16];
+    char hex_output[16*2 + 1];
+    int di;
+    
+    md5_init(&HASH);
+    md5_append(&HASH, (const md5_byte_t *)input_seed, strlen(input_seed));
+    md5_finish(&HASH, digest);
+    
+    for (di = 0; di < 16; ++di) {
+	    sprintf(hex_output + di * 2, "%02x", digest[di]);
+    }
+    return hex_output;
 }
---
+++ src/gen.h
@@ -0,0 +1,13 @@
+#ifndef GEN_H
+#define GEN_H
+
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "../md5.h"
+
+char* generator_template(char* seed, char *input);
+char* generator_default(char* seed, char *input, int complexity);
+char* hash(char *input_seed);
+
+#endif
