4f628284f2da47858dd6789424dc41f88fee9128
Roman Mingazeev <Direnol@yandex.ru>
2016-04-21 02:35:10 +0600

    Сделяль частично, генерирует разные символы, не реализован генератор с неизменной частью

--- src/console/gen.c
+++ src/console/gen.c
@@ -1,41 +1,41 @@
 #include "gen.h"
 
-char *generator_template(char* seed, char *input)
+char *generator_template(char *input)
 {
-	char A[] = {'0','1','2','3','4','5','6','7','8','9'};
-	char B[] = {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
-	char C[] = {'!','@','#','$','%','^','&','*','(',')'};
-	char D[] = {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'};
+	char NUM[] = {'0','1','2','3','4','5','6','7','8','9'};
+	char UP[] = {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
+	char SYM[] = {'!','@','#','$','%','^','&','*','(',')'};
+	char DOWN[] = {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'};
 	
 	char *out = malloc(sizeof(char) * strlen(input));
-    int i;
-
-    FILE *file_data;
-    file_data = fopen("PATH", "r");
-    char data[1024];
-    fgets(data, 1024, file_data);
-        
-    char *h = hash(file_data);
+    int i, j = 0;
+    char *str_data = malloc(sizeof(char) * size_data);
+    char *h = NULL;
+    data(&str_data);
+    hash(str_data, &h);
     int seed;
     
     for (i = 0; i < strlen(input); i++) {
-        switch (inputp[i]) {
-            case C:
-                seed = h[i % strlen(input)] * wtime() % strlen(B);
-                out[i] = B[seed];
+        switch (input[i]) {
+            case 'C':
+                seed = h[(i + j % strlen(input))] % strlen(UP);
+                out[i] = UP[seed];
+                j++;
                 break;
-            case c:
-                seed = h[i % strlen(input)] * wtime() % strlen(D);
-                out[i] = D[seed];
+            case 'c':
+                seed = h[(i +j % strlen(input))] % strlen(DOWN);
+                out[i] = DOWN[seed];
+                j++;
                 break;
-            case d:
-                seed = h[i % strlen(input)] * wtime() % strlen(A);
-                out[i] = A[seed];
+            case 'd':
+                seed = h[(i  + j % strlen(input))] % strlen(NUM);
+                out[i] = NUM[seed];
+                j++;
                 break;
-            case s:
-                seed = h[i % strlen(input)] * wtime() % strlen(s);
-                out[i] =C[seed];
-            
+            case 's':
+                seed = h[(i + j % strlen(input))] % strlen(SYM);
+                out[i] =SYM[seed];
+                j++;
         }
     }
 	return out;
@@ -43,21 +43,68 @@ char *generator_template(char* seed, char *input)
 
 char *generator_default(char *input, int complexity)
 {
+    return "Don't ready";
 }
 
-char *hash(char *input_seed);
+void data(char **str)
 {
-    md5_state_t HASH;
-    md5_byte_t digest[16];
-    char hex_output[16*2 + 1];
-    int di;
+    char time[32]; // pointer on time
+    char buf[size_data]; // the firsh use to Mhz
+    char name[64]; // login
+    char ip[32]; // IP
     
-    md5_init(&HASH);
-    md5_append(&HASH, (const md5_byte_t *)input_seed, strlen(input_seed));
-    md5_finish(&HASH, digest);
-    
-    for (di = 0; di < 16; ++di) {
-	    sprintf(hex_output + di * 2, "%02x", digest[di]);
+    system("who >buf.txt");
+    system("ifconfig > buf2.txt");
+
+    // Found user's login +
+    FILE *foo;
+    foo = fopen("buf.txt", "r");
+
+    fscanf(foo, "%s\n", name);
+    // Found user's login -
+
+    // Found ip +
+    foo = fopen("buf2.txt", "r");
+
+    while(strcmp(ip, "inet") != 0) {
+        fscanf(foo, "%s\n",  ip);
     }
-    return hex_output;
+    
+    fscanf(foo, "%s\n", ip);
+    sprintf(ip, "%s", (ip + 5)); // Delete "addr"
+    //Found ip -
+
+    // Found MHz +
+    FILE *file_proc;
+    file_proc = fopen("/proc/cpuinfo", "r");
+
+     while (strcmp(buf, "MHz") != 0) {
+         fscanf(file_proc, "%s", buf);
+     }
+     
+     fscanf(file_proc, "%s", buf);
+     fscanf(file_proc, "%s", buf);
+    // Found MHz -
+
+    // printf("MHZ %s\n", buf);
+    // printf("Name %s\n", name);
+    // printf("Ip %s\n", ip);
+    sprintf(time, "%f", wtime());
+
+    strcat(buf, name);
+    strcat(buf, ip);
+    strcat(buf, time);
+
+    //printf("Reuslt %s\n", buf);
+    strcpy(*str, buf);
+    system("rm buf.txt buf2.txt");
+    fclose(foo);
+    fclose(file_proc);
+}
+
+double wtime()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
 }
--- src/console/gen.h
+++ src/console/gen.h
@@ -2,13 +2,17 @@
 #define GEN_H
 
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 #include <string.h>
 #include "md5.h"
-#include "fanc.h"
+#include "hash.h"
+
+#define size_data 256
 
 char* generator_template(char *input);
 char* generator_default(char *input, int complexity);
-char* hash(char *input_seed);
+void data(char **str);
+double wtime();
+
 
 #endif
