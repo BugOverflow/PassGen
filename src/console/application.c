c95a3289336cf2031cbe6f74362bb814fedd6efc
Valery Agishev <phaseowner@gmail.com>
2016-04-20 15:59:20 +0600

    Мерж

--- src/console/application.c
+++ src/console/application.c
@@ -1,46 +1,97 @@
 #include "application.h"
 
-PassGenOptions options = { MODE_DEFAULT, 0, NULL };
+PassGenOptions options = { MODE_DEFAULT, 0, 0, NULL };
+PassGenError err = { ERR_NO, "" };
 
 int run(int argc, char **argv)
 {
-	parse_options(argc, argv);
-	
-	return 0;
+    parse_options(argc, argv);
+
+    fprintf(stderr, "Err: %d, %s \n", err.code, err.message);
+
+    return 0;
+}
+
+void handle_err(PassGenErrCode code, char *mess)
+{
+    err.code = code;
+    strcpy(err.message, mess);
 }
 
 void parse_options(int argc, char **argv)
 {
-	//p_opt->mode = MODE_DEFAULT;
-	//p_opt->count = 0;
-    //p_opt->template = NULL;
-    
-	//int opt;
-	//opterr = 0; // запретить вывод ошибок от getopt()
-	//while ((opt = getopt(argc, argv, "hvt:dc:")) != -1)
-	//{
-		//switch (opt)
-		//{
-            //default: /* '?' */
-			//case 'h':
-				//printf("Basic usage: \"PassGen \"\n");
-				//break;
-			//case 'v':
-				//printf("Version 0.0\n");
-				//break;
-            //case 't':
-                //p_opt->mode = MODE_TEMPLATE;
-                //// шаблон
-                //p_opt->template = malloc(sizeof(char) * strlen(optarg));
-                //strncpy(p_opt->template, optarg, strlen(optarg));
-                //break;
-            //case 'd':
-                //p_opt->mode = MODE_DEFAULT;
-                //break;
-			//case 'c':
-				//// количество символов
-				//p_opt->count = atoi(optarg);
-				//break;
-		//}
-	//}
+    if (argc < 2) {
+        handle_err(ERR_PARSING, "Need more arguments.");
+        return;
+    }
+
+    const char *KEYS = "t:ds:c:";       //arg keys
+    opterr = 0;
+
+    int c;
+    while ((c = getopt(argc, argv, KEYS)) != -1) {
+        switch (c) {
+            case 't':          // template mode
+                {
+                    if ((options.mode == MODE_DEFAULT) &&
+                        (options.pass_size == 0) &&
+                        (options.pass_strength == 0)) {
+                        options.mode = MODE_TEMPLATE;
+                        options.template = optarg;
+                    } else
+                        handle_err(ERR_PARSING,
+                                   "Too many arguments for template mode! Please, try again.");
+                    break;
+                }
+            case 'd':          // default mode
+                {
+                    if (options.mode == MODE_DEFAULT)
+                        options.mode = MODE_DEFAULT;
+                    else
+                        handle_err(ERR_PARSING,
+                                   "Unknown mode! Please, try again.");
+                    break;
+                }
+            case 's':          // password difficulty
+                {
+                    if (options.mode != MODE_TEMPLATE) {
+                        int tmp = atoi(optarg);
+                        if (tmp > 0)
+                            options.pass_strength = tmp;
+                        else
+                            handle_err(ERR_PARSING,
+                                       "Wrong value of strong mode! Please, try again.");
+                    } else
+                        handle_err(ERR_PARSING,
+                                   "Too many arguments for template mode! Please, try again.");
+                    break;
+                }
+            case 'c':          // count of password symbols
+                {
+                    if (options.mode != MODE_TEMPLATE) {
+                        int tmp = atoi(optarg);
+                        if (tmp > 0)
+                            options.pass_size = tmp;
+                        else
+                            handle_err(ERR_PARSING,
+                                       "Wrong value of password lenght! Please, try again.");
+                    } else
+                        handle_err(ERR_PARSING,
+                                   "Too many arguments for template mode! Please, try again.");
+                    break;
+                }
+            case '?':
+                {
+                    if (optopt == 'c' || optopt == 't')
+                        handle_err(ERR_PARSING,
+                                   "This option requires an argument.");
+                    else if (isprint(optopt))
+                        handle_err(ERR_PARSING, "Unknown option.");
+                    else
+                        handle_err(ERR_PARSING, "Unknown option character.");
+                }
+            default:
+                break;
+        }
+    }
 }
--- src/console/application.h
+++ src/console/application.h
@@ -3,6 +3,9 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef enum {
     MODE_DEFAULT = 0,
@@ -11,11 +14,23 @@ typedef enum {
 
 typedef struct {
     PassGenMode mode;
-	int pass_size;
+    int pass_size;
+    int pass_strength;
     char *template;
 } PassGenOptions;
 
+typedef enum {
+    ERR_NO = 0,
+    ERR_PARSING
+} PassGenErrCode;
+
+typedef struct {
+    PassGenErrCode code;
+    char message[128];
+} PassGenError;
+
 int run(int argc, char **argv);
+void handle_err(PassGenErrCode code, char *mess);
 void parse_options(int argc, char **argv);
 
 #endif
