6e1af826aeba6d31e789766659b0bdf6794205be
Roman Mingazeev <Direnol@yandex.ru>
2016-04-20 18:59:01 +0600

    Создание gen.c: gen_template; Создание Hash

---
+++ src/console/gen.h
@@ -0,0 +1,14 @@
+#ifndef GEN_H
+#define GEN_H
+
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "md5.h"
+#include "fanc.h"
+
+char* generator_template(char *input);
+char* generator_default(char *input, int complexity);
+char* hash(char *input_seed);
+
+#endif
