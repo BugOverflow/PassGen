94b3ecfc26fe28a55f1cf4b3ead367025e610b54
nick-ugolnikov <nickorussisch@gmail.com>
2016-04-24 17:51:48 +0600

    default generator: first stable version with a standart randomizer

---
+++ Pass test/default.c
@@ -0,0 +1,42 @@
+#include "liters.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/* getrand: Returns random number from [min, max) */
+int getrand(int min, int max)
+{
+    return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
+}
+
+void defaultGen(Data options, *out)
+{
+	char pass[options.count];
+	int k, index;
+
+	for (short int i = 0; i < options.count; ++i)
+	{
+		k = i % options.strong;
+
+		switch (k)
+		{
+			case 0:
+				index = getrand(0, sizeLowerCase);
+				pass[i] = lowerCase[index];
+				break;
+			case 1:
+				index = getrand(0, sizeUpperCase);
+				pass[i] = upperCase[index];
+				break;
+			case 2:
+				index = getrand(0, sizeDigits);
+				pass[i] = digits[index];
+				break;
+			case 3:
+				index = getrand(0, sizeSymbols);
+				pass[i] = symbols[index];
+				break;
+		}
+	}
+
+	out = pass;
+}
--- src/console/application.c
+++ src/console/application.c
@@ -1,4 +1,5 @@
 #include "application.h"
+#include "liters.h"
 
 PassGenOptions options = { MODE_DEFAULT, 0, 0, NULL };
 PassGenError err = { ERR_NO, "" };
@@ -8,6 +9,14 @@ int run(int argc, char **argv)
     parse_options(argc, argv);
 
     fprintf(stderr, "Err: %d, %s \n", err.code, err.message);
+
+    char word[options.pass_size];
+    default_gen(options, word);
+    printf("%s\n", word);
+    FILE *output = fopen("test.txt", "w");
+    fprintf(output, "%s", word);
+    fclose(output);
+
 
     return 0;
 }
@@ -94,4 +103,49 @@ void parse_options(int argc, char **argv)
                 break;
         }
     }
+}
+
+
+
+
+
+
+
+
+/* getrand: Returns random number from [min, max) */
+int get_rand(int min, int max)
+{
+    return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
+}
+
+void default_gen(PassGenOptions data, char *pass)
+{
+    int k, index;
+
+    for (int i = 0; i < data.pass_size; ++i)
+    {
+        k = i % data.pass_strength;
+
+        switch (k)
+        {
+            case 0:
+                index = get_rand(0, sizeLowerCase - 1) % sizeLowerCase;
+                pass[i] = lowerCase[index];
+                break;
+            case 1:
+                index = get_rand(0, sizeUpperCase - 1) % sizeLowerCase;
+                pass[i] = upperCase[index];
+                break;
+            case 2:
+                index = get_rand(0, sizeDigits - 1) % sizeDigits;
+                pass[i] = digits[index];
+                break;
+            case 3:
+                index = get_rand(0, sizeSymbols - 1) % sizeSymbols;
+                pass[i] = symbols[index];
+                break;
+        }
+    }
+    
+    //pass[data.pass_size + 1] = '\n';
 }
