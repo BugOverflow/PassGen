6105430c2ec54691b9e1a4f3a85e8358369a063a
jonn <evg.kazartseff@yandex.ru>
2016-05-14 13:12:10 +0600

    test parsing

--- src/test/parsing_test.c
+++ src/test/parsing_test.c
@@ -1,15 +1,55 @@
 #include "ctest.h"
 #include "../console/parsing.h"
 
-CTEST(parsing, test_1)
+CTEST(parsing, return_test)
 {
     char *input = malloc(80 * sizeof(char));
     char *output;
     int flg;
     
-    input = "C4s3d2c1d3c2?qwert?";
+    input = "C4s3d2c1d3c2?qwert?2";
     
     flg = parsing(input, &output);
+    ASSERT_STR("CCCCsssddcdddcc?qwert??qwert?", output);
+    ASSERT_EQUAL(0, flg);
+    free(input);
+}
+
+CTEST(parsing, err_key_test)
+{
+    char *input = malloc(80 * sizeof(char));
+    char *output = NULL;
+    int flg;
     
+    input = "cdr";
     
+    flg = parsing(input, &output);
+    ASSERT_NULL(output);
+    ASSERT_EQUAL(-3, flg);
+    free(input);
+}
+
+CTEST(parsing, NULL_input_test)
+{
+    char *input = NULL;
+    char *output = NULL;
+    int flg;
+    
+    flg = parsing(input, &output);
+    ASSERT_NULL(output);
+    ASSERT_EQUAL(-1, flg);
+}
+
+CTEST(parsing, no_question_key_test)
+{
+    char *input = malloc(80 * sizeof(char));
+    char *output = NULL;
+    int flg;
+    
+    input = "C4s3d2c1d3c2?qwert";
+    
+    flg = parsing(input, &output);
+    ASSERT_EQUAL(-2, flg);
+    ASSERT_NULL(output);
+    free(input);
 }
