5e6717c9b3491f4ca9b380e2977ee458d5070b00
jonn <evg.kazartseff@yandex.ru>
2016-05-14 12:18:28 +0600

    исправил ошибки

--- src/test/parsing_test.c
+++ src/test/parsing_test.c
@@ -1,7 +1,15 @@
-#include "stest.h"
-#include "../src/parsing.s"
+#include "ctest.h"
+#include "../console/parsing.h"
 
 CTEST(parsing, test_1)
 {
+    char *input = malloc(80 * sizeof(char));
+    char *output;
+    int flg;
+    
+    input = "C4s3d2c1d3c2?qwert?";
+    
+    flg = parsing(input, &output);
+    
     
 }
