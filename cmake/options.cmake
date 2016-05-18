b4743496272a9a89e962c8c23a5a7135df6e9b25
Valery <phaseowner@gmail.com>
2016-05-18 14:50:27 +0600

    CMake: fix options

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,4 +1,4 @@
 option(BUILD_GUI "Build gui for PassGen" 0)
-option(QT_PATH "Path to Qt5 libs" 0)
+set(QT_PATH "~/Qt/5.5/gcc" CACHE STRING "Path to Qt5 libs")
 option(WITH_WARNINGS "Show all warnings during compile" 0)
-option(BUILD_TEST "Build test for RassGen" 0)
+option(BUILD_TEST "Build test for PassGen" 0)
