080fec65416a94644e16b12af01c22d7a012f240
jenya-meow <jeny97@ngs.ru>
2016-06-04 00:16:39 +0600

    fix

--- src/console/config.h
+++ src/console/config.h
@@ -4,22 +4,6 @@
 #include <string.h>
 #include "application.h"
 
-//поиск символа в строке
-int schr(char str[], char ch);
-
-//разбиение строки на поля
-int stok(char str[], char delim, int *ptr[]);
-
-//посимвольное сравнивание строк
-int sequal(char s1[], char s2[]);
-
-//поиск подстроки в строке
-int sstr(char txt[], char p[]);
-
-//копирование строки в строку
-void copy(char s1[], char s2[]);
-
-//значение параметра
 char *returnvalue(char *str, char *mode);
 
 void get_template(char str[], char mode[], char value[]);
